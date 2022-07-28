a2115f699d6f73b3d887be484e2ab264fb81ed7d
dori-dev <mr.dori.dev@gmail.com>
2022-07-28 12:28:24 +0430

    add slash  to end of notes:detail url path

--- notes/urls.py
+++ notes/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'notes'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('detail/<int:pk>', views.detail, name='detail')
+    path('detail/<int:pk>/', views.detail, name='detail')
 ]

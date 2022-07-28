f1f13c14f79516fe26e11988560b5fd7ee611c54
dori-dev <mr.dori.dev@gmail.com>
2022-07-28 11:19:46 +0430

    create the note model and register it in the admin

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Note
 
-# Register your models here.
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'id', 'author', 'created')
+    list_filter = ('author',)
+    search_fields = ('title', 'body', 'author__username')
+
+
+admin.site.register(Note, NoteAdmin)
--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(
+        max_length=128
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        blank=False, null=False
+    )
+    body = models.TextField()
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self) -> str:
+        return self.title

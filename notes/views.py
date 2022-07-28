979b2dd3b297ba940b42122b3edb730b5c4e32f7
dori-dev <mr.dori.dev@gmail.com>
2022-07-28 11:37:06 +0430

    add index page

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.handlers.wsgi import WSGIRequest
+from .models import Note
 
 
-def index(request):
-    return HttpResponse('notes index')
+def index(request: WSGIRequest):
+    if request.user.is_anonymous:
+        notes = None
+    else:
+        notes = Note.objects.filter(
+            author=request.user
+        ).order_by('-created')
+    context = {
+        "notes": notes
+    }
+    return render(request, "notes/index.html", context)
